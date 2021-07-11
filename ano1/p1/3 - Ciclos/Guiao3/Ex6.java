f33b6507a61882614b8df74d632ae2cf3cf456d8
Dário Matos <45235143+Dariod9@users.noreply.github.com>
2021-07-11 18:02:41 +0100

    Delete ano1 directory

--- ano1/p1/3 - Ciclos/Guiao3/Ex6.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Ex5
-{
-  public static void main(String[] args) {
-
-	Scanner sc= new Scanner (System.in);
-
-double a,b,r,MDC1,MDC2;
--- ano1/p1/9 - Classes/G8Ex1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class G8Ex1 {
-	static Scanner sc = new Scanner (System.in);
-	public static void main (String[] args) {
-
-	
- }
-}
